ed55dd461f153e082d4ce4422489e3247eb58305
vlee18 <43415065+vlee18@users.noreply.github.com>
2019-10-24 21:59:13 -0400

    Add files via upload
    The bug with being able to double jump after walking on a platform should be fixed. Also fixed a bug with the character "sticking" to a platform if hit a platform from below while jumping.

--- Glib/Camera2D.gd
+++ Glib/Camera2D.gd
@@ -1,10 +1,1 @@
 extends Camera2D
-var offset1 = Vector2(900,0)
-var offset2 = Vector2(0,0)
-var offset3 = Vector2(-900,0)
-func _on_KinematicBody2D_changecameraleft():
-	set_offset(offset1)
-func _on_KinematicBody2D_changecameracenter():
-	set_offset(offset2)
-func _on_KinematicBody2D_changecameraright():
-	set_offset(offset3)
--- Glib/KinematicBody2D.gd
+++ Glib/KinematicBody2D.gd
@@ -1,12 +1,10 @@
  extends KinematicBody2D
-var grav = Vector2(0,5)
-var left = Vector2(-6,0)
-var right = Vector2(6,0)
-var jump = Vector2(0,-9)
-var leftborder = 570.0
-signal changecameraleft
-signal changecameraright
-signal changecameracenter
+var gravity = Vector2(0,1) # -x = left, +x = right, -y = up , +y = down. The arguments are Vector2(x,y).
+var left = Vector2(-10,0)
+var right = Vector2(10,0)
+var up = Vector2(0,-16)
+var floors = Vector2(0,-1) #Normal vector to describe what surfaces are considered floors. 
+var jumpcheck = 0 #Checks to see if the player has pressed the jump button. 
 func moveleft():
 	if  Input.is_action_pressed("ui_left"):
 		move_and_collide(left)
@@ -14,19 +12,22 @@ func moveright():
 	if Input.is_action_pressed("ui_right"):
 		move_and_collide(right)
 func jump():
-	if Input.is_action_pressed("ui_up"):
-		move_and_collide(jump)
-func _physics_process(delta):
-	if get_position().x >= (leftborder):
-		emit_signal("changecameraleft")
-		if get_position().x <= (leftborder):
-			emit_signal("changecameracenter")
-	if get_position().x <= (-540):
-		emit_signal("changecameraright")
-		if get_position().x >= (-540):
-			emit_signal("changecameracenter")
-	move_and_collide(grav)
+	if Input.is_action_just_pressed("ui_up"): #If the jump button is pressed and the player is standing on the ground, jumpcheck is set to 1, and the up vector is applied to the glib.
+		if(is_on_floor()):
+			jumpcheck = 1
+			move_and_collide(up)
+func bumped(body): #If glib hits another platform while jumping, it will stop the jump momentum.
+	jumpcheck = 0
+func _physics_process(delta):  
+	move_and_slide(gravity,floors) #Argument 1 is to move the body a certain direction, Argument 2 is the floor vector. The gravity vector is applied to glib  
 	moveright()
 	moveleft()
 	jump()
-	print(str(get_position()))
+	if(!is_on_floor()): #If player is off the ground, then gravity increases while off the ground. 
+		for x in range (0,10000):
+			gravity.y += 0.004
+	if(!is_on_floor() && jumpcheck == 1): #If the player if off the ground, and had pressed the jump button, the up vector is applied to glib, while also in the opposite direction of the gravity vector, which is increasing.
+		move_and_collide(up)
+	if(is_on_floor() && !Input.is_action_just_pressed("ui_up")): #When player lands, gravity and jumpcheck are both reset.
+		jumpcheck = 0
+		gravity.y = 1
