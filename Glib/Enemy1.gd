e07e2db604ecba0e56f388eeded1b1b4ea6080af
vlee18 <43415065+vlee18@users.noreply.github.com>
2019-11-30 23:51:14 -0500

    Add files via upload
    Added animation to walking enemy.

--- Glib/Enemy1.gd
+++ Glib/Enemy1.gd
@@ -1,11 +1,22 @@
 extends KinematicBody2D
 var gravity = Vector2(0,1)
 var floors = Vector2(0,-1)
-var enemy1move = Vector2(4,0)
+var enemy1move = Vector2(-4,0)
+var left = 1
 func turn():
 	enemy1move.x = -enemy1move.x
+	left = -left 
 func enemy1mover():
 	move_and_collide(enemy1move)
+func _ready():
+	get_node("EnemyWalkRight").hide()
+func _process(delta):
+	if(left == 1):
+		get_node("EnemyWalkRight").hide()
+		get_node("EnemyWalkLeft").set_visible(true)
+	if(left == -1):
+		get_node("EnemyWalkRight").set_visible(true)
+		get_node("EnemyWalkLeft").hide()
 func _physics_process(delta):
 	move_and_slide(gravity,floors)
 	if(!is_on_floor()): 
